b731a1f96ce8bf99bbbbc0be9a0110a360d6c80b
kimYejin <kimphoby0125@gmail.com>
2024-03-28 23:22:43 +0900

    refactor service name

--- src/main/java/GDG/whatssue/domain/member/controller/ClubMemberController.java
+++ src/main/java/GDG/whatssue/domain/member/controller/ClubMemberController.java
@@ -1,6 +1,6 @@
 package GDG.whatssue.domain.member.controller;
 
-import GDG.whatssue.domain.member.service.ClubMemberService;
+import GDG.whatssue.domain.member.service.ClubMemberManagingService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/clubs/{clubId}/members")
 public class ClubMemberController {
 
-    private final ClubMemberService clubMemberService;
+    private final ClubMemberManagingService clubMemberService;
     @DeleteMapping("/{memberId}/manager")
     @PreAuthorize("hasAnyRole('ROLE_'+#clubId+'MANAGER')")
     @Operation(summary = "멤버 추방", description = "멤버를 클럽에서 추방합니다.(매니저만 이용 가능한 기능)")
