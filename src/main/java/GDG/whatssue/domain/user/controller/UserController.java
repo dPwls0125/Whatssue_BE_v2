8c01bc72f46b300e0a97cb1d8bc385f5de501223
kimYejin <kimphoby0125@gmail.com>
2024-05-19 20:30:45 +0900

    refactor : oauth login successful handler

--- src/main/java/GDG/whatssue/domain/user/controller/UserController.java
+++ src/main/java/GDG/whatssue/domain/user/controller/UserController.java
@@ -2,12 +2,8 @@ package GDG.whatssue.domain.user.controller;
 
 import GDG.whatssue.domain.user.dto.SignUpRequestDto;
 import GDG.whatssue.domain.user.dto.UserDto;
-//import GDG.whatssue.domain.user.entity.PrincipalDetails;
-//import GDG.whatssue.domain.user.service.UserService;
 import GDG.whatssue.domain.user.entity.KakaoDetails;
 import GDG.whatssue.domain.user.service.CustomOauth2Service;
-import GDG.whatssue.global.common.annotation.LoginUser;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @Slf4j
@@ -44,10 +39,4 @@ public class  UserController {
         return ResponseEntity.status(200).body(userDto);
     }
 
-    @GetMapping("/login/redirect")
-    @Operation(summary = "로그인 이후 적절한 화면으로 리다이렉트", description = "회원가입을 안한 유저인 경우 -> signUp page\n 회원가입을 한 유저인 경우 -> main page")
-    public RedirectView loginRedirect(@LoginUser Long userId){
-        RedirectView redirect = customOauth2Service.loginRedirect(userId);
-        return redirect;
-    }
 }
