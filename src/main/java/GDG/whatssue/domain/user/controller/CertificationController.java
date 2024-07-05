b6200fb8bba5a89c9648ce132954a9c2f188591d
kimYejin <kimphoby0125@gmail.com>
2024-07-05 15:16:51 +0900

    fix: 인증번호 api  수정

--- src/main/java/GDG/whatssue/domain/user/controller/CertificationController.java
+++ src/main/java/GDG/whatssue/domain/user/controller/CertificationController.java
@@ -28,8 +28,8 @@ public class CertificationController {
     @Operation(summary = "인증 번호 확인")
     public ResponseEntity checkRandomNumber(@RequestParam("toNumber") String toNumber, @RequestParam("certificationNum") int certificationNum, @AuthenticationPrincipal KakaoDetails kakaoDetails) {
         Long userId = kakaoDetails.getUser().getUserId();
-        Boolean result = myMessageService.checkCertNum(toNumber, certificationNum, userId);
-        return ResponseEntity.status(200).body(result);
+         myMessageService.checkCertNum(toNumber, certificationNum, userId);
+        return ResponseEntity.status(200).body("인증 완료");
     }
 }
 
