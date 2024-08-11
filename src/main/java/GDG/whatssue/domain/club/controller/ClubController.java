c542d129521660760864f0f63f51e25d5b775e37
Ji-minhyeok <gmh8257@gmail.com>
2024-08-11 17:56:00 +0900

    Feat: 모임 삭제 기능 구현

--- src/main/java/GDG/whatssue/domain/club/controller/ClubController.java
+++ src/main/java/GDG/whatssue/domain/club/controller/ClubController.java
@@ -26,15 +26,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -132,9 +124,11 @@ public class ClubController {
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
-//    @DeleteMapping
-//    public ResponseEntity deleteClub() {
-//        //TODO
-//        return null;
-//    }
+    @ClubManager
+    @Operation(summary = "모임 삭제")
+    @DeleteMapping("/{clubId}/delete")
+    public ResponseEntity deleteClub(@PathVariable("clubId") Long clubId){
+        clubService.deleteClub(clubId);
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
 }
